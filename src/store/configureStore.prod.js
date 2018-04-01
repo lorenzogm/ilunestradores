f6aef2b6013e01dd73ae7deae96ace7923dae443
Lorenzo GM <dev@lorenzogm.com>
2018-04-01 14:01:29 +0200

    Remove "no-unused-vars"

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 
